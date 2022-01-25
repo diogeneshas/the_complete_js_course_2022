ea65126a527af7f0299cc346bccbf7b43fe36342
diogeneshas <diogenes.henrique.br@gmail.com>
2022-01-25 09:45:46 -0300

    feat: Exercicios parte 1 - Values and Variables

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,10 @@
+console.log("==============================================")
+console.log('Exercicios do JavaScript Fundamentals - Part 1')
+console.log("==============================================")
+let country = 'Brasil'
+let continent = 'America do Sul'
+let population = 213000000
+
+console.log(country)
+console.log(continent)
+console.log(population)
