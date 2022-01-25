03f52dedd1837437cac2e0454eaba93ca32c0698
diogeneshas <diogenes.henrique.br@gmail.com>
2022-01-25 10:00:55 -0300

    feat: Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -3,3 +3,10 @@ console.log(40 + 8 + 23 - 10)
 
 let firstName = "Jonas"
 console.log(firstName)
+
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+
+console.log(typeof true)
+console.log(typeof javascriptIsFun)
+console.log(typeof 23)
