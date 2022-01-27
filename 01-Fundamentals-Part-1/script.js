87895408b098d168defe96d7f81b19c25e58c3ae
diogeneshas <diogenes.henrique.br@gmail.com>
2022-01-27 06:35:54 -0300

    feat: Basic Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -15,3 +15,32 @@ let age = 30
 age = 31
 
 const birthYear = 1991
+
+
+console.log("==== Basic Operators ====")
+const now = 2037
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+console.log(ageJonas)
+console.log(ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+
+firstName = 'Jonas'
+const lastName = 'Schmedtmann'
+console.log(firstName + ' ' + lastName)
+
+let x = 10 + 5
+x += 10 // x = x + 10
+x *= 4 // x = x * 4
+x++ // x = x + 1
+x--
+x--
+console.log(x)
+
+console.log(ageJonas > ageSarah)
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+
+console.log(now - 1991 > now - 2018)
